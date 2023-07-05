fe99027fb6b8a0270d9e1615be46e6c7051be2ca
zanwhip <hungnv.21it@vku.udn.vn>
2023-07-05 16:42:35 +0700

    Kết Nối Tới MySQL Database với Sequelize - Render Dữ Liệu Ra View

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,21 @@
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs')
+import db from '../models/index'
+
+
+let getHomePage = async(req, res) => {
+    try {
+        let data = await db.User.findAll();
+       console.log('-------------')
+        console.log(data) 
+        console.log('-------------')
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+         }
+        )
+    } catch (e) {
+        console.log(e)
+    }
+
+   
 }
 let getAboutPage = (req, res) => {
     return res.render('test/about.ejs')
